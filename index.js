62a8e595734ca51be4118a1af03c874bf42dbc2c
Tyler <TylerProbst@Tylers-MacBook-Pro.local>
2016-01-19 07:35:27 -0800

    adds analytics

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     
     $(window).scroll( function () {
         console.log($(window).scrollTop());
-        if ($(window).scrollTop() >= 751) {
+        if ($(window).scrollTop() >= 755) {
             $('.projects').addClass('project-drop');
             $('#navbar').addClass('navbar-right');
         }
-        if ($(window).scrollTop() < 754) {
+        if ($(window).scrollTop() < 759) {
             $('.projects').removeClass('project-drop');
             $('#navbar').removeClass('navbar-right');
         }
