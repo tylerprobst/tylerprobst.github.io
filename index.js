d853f5b1c294a13e99ea3b4ee0f1be8a751ae3fa
Tyler <TylerProbst@Tylers-MacBook-Pro.local>
2016-02-11 08:29:42 -0800

    changes navbar operation and more styling

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     
     $(window).scroll( function () {
         console.log($(window).scrollTop());
-        if ($(window).scrollTop() >= 755) {
+        if ($(window).scrollTop() >= 763) {
             $('.projects').addClass('project-drop');
             $('#navbar').addClass('navbar-right');
         }
-        if ($(window).scrollTop() < 759) {
+        if ($(window).scrollTop() < 767) {
             $('.projects').removeClass('project-drop');
             $('#navbar').removeClass('navbar-right');
         }
